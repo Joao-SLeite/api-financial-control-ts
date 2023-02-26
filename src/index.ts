a241b0dc83ea1db263a01a29d8588f885f805a4f
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2023-02-26 17:00:35 -0300

    database connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('object');
+import express from 'express';
+import { AppDataSource } from './data-source';
+
+AppDataSource.initialize().then(() => {
+    const app = express();
+
+    app.use(express.json());
+
+    app.get('/', (req, res) => {
+        return res.json({ message: 'Servidor em execução' });
+    });
+
+    return app.listen(process.env.PORT);
+});
